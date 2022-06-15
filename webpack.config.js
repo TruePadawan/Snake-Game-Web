47fc6817161d86a9f78a0de84973c4412adcef39
Hermes <hermeschigoziri@gmail.com>
2022-06-15 14:39:49 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
+  mode: "development",
   devtool: "inline-source-map",
   devServer: {
-      static: './dist',
+    static: "./dist",
   },
   output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "dist"),
-  }
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
