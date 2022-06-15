1719664e89d3f71f6e918b1f7522bbeb4f3ca839
Hermes <hermeschigoziri@gmail.com>
2022-06-15 14:46:57 +0100

    Update food.test.js

--- tests/food.test.js
+++ tests/food.test.js
@@ -3,17 +3,14 @@ import { Food } from "../src/food";
 import { Snake } from "../src/snake";
 
 test("Food object can respawn, which should change its coordinate", () => {
-    const food = new Food(0, 0);
+    const food = new Food();
     const coord = new Coordinate(0, 0);
     food.respawn();
     expect(coord.equal(food.getCoordinate())).not.toBeTruthy();
 });
 
-
-test("Food object' isEaten method should return true when snake is at the same coord as it", () => {
-    const food = new Food(10, 3);
-    const snake = new Snake();
-    snake.move(new Coordinate(0, 1));
-    
-    expect(food.isEaten(snake.getHeadCoordinate())).toBeTruthy();
-});
+test("Food object are created on random positions", () => {
+    const food1 = new Food();
+    const food2 = new Food();
+    expect(food1.getCoordinate().equal(food2.getCoordinate())).toBeFalsy();
+})
